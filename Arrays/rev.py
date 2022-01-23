dd481c72e5a48189831c2d234c485932ef74a4f7
Aditya_Sai <2004sai@gmail.com>
2022-01-23 20:03:56 +0530

    Update rev.py

--- Arrays/rev.py
+++ Arrays/rev.py
@@ -1,5 +1,12 @@
+#using inbuilt functions
 n=int(input())
 li=list(map(int,input().split(' ')))
 li.reverse()
 for i in li:
     print(i,end=' ')
+
+#def function
+#separate code
+
+def rev(li):
+    return li[::-1]
